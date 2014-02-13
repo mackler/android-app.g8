8688127a80911ce3bdc023a89b35f785e21d7eb0
Adam Mackler <github@mackler.org>
2014-02-13 02:20:31 -0500

    Update for use with sbt-android plugin version 0.7

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -1,56 +1,47 @@
+// required SBT components
 import sbt._
-
 import Keys._
-import sbtandroid.AndroidKeys._
+import Defaults._
 
-object General {
-  val settings = Defaults.defaultSettings ++ Seq (
+// SBT-Android keys and helper methods
+import sbtandroid.AndroidPlugin._
+
+object AndroidBuild extends Build {
+
+  val globalSettings = Seq (
     name := "$name$",
     version := "0.1",
     versionCode := 0,
     scalaVersion := "$scala_version$",
-    platformName in Android := "android-$api_level$",
-    scalacOptions in Compile ++= Seq("-deprecation","-feature","-language:implicitConversions","-unchecked"),
+    platformName := "android-$api_level$"",
+    useProguard := $useProguard$,
+    proguardOptions += "@project/proguard.cfg",
+    keyalias := "change-me",
+    libraryDependencies ++= Seq(
+      "org.scalatest" %% "scalatest"     % "$scalatest_version$" % "test",
+      "org.slf4j"      % "slf4j-android" % "1.7.6"
+    ),
+    scalacOptions in Compile ++= Seq(
+      "-deprecation","-feature","-language:implicitConversions","-unchecked"
+    ),
+    javacOptions ++= Seq("-encoding", "UTF-8", "-source", "1.6", "-target", "1.6"),
     javaOptions in Compile += "-Dscalac.patmat.analysisBudget=off",
     initialize ~= { _ ⇒
       sys.props("scalac.patmat.analysisBudget") = "512"
     }
   )
 
-  val proguardSettings = Seq (
-    useProguard in Android := $useProguard$,
-    proguardOption in Android := "@project/proguard.cfg"
+  lazy val main = AndroidProject (
+    id       = "$name$",
+    base     = file("."),
+    settings = globalSettings
   )
 
-  lazy val fullAndroidSettings =
-    General.settings ++
-    sbtandroid.AndroidProject.androidSettings ++
-    sbtandroid.TypedResources.settings ++
-    proguardSettings ++
-    sbtandroid.AndroidManifestGenerator.settings ++
-    sbtandroid.AndroidMarketPublish.settings ++ Seq (
-      keyalias in Android := "change-me",
-      libraryDependencies ++= Seq(
-        "org.scalatest" %% "scalatest"     % "$scalatest_version$" % "test",
-        "org.slf4j"      % "slf4j-android" % "1.6.1-RC1"
-      )
-    )
-}
-
-object AndroidBuild extends Build {
-  lazy val main = Project (
-    id = "$name$",
-    base = file("."),
-    settings = General.fullAndroidSettings
-  )
-
-  lazy val tests = Project (
-    id = "tests",
-    base = file("tests"),
-    settings = General.settings ++
-               sbtandroid.AndroidTest.androidSettings ++
-               General.proguardSettings ++ Seq (
-      name := "$name$Tests"
-    )
-  ) dependsOn main
+  lazy val tests = AndroidTestProject (
+    id       = "tests",
+    base     = file("tests"),
+    settings = globalSettings
+  ).
+  dependsOn(main % "provided").
+  settings(name := "$name$Tests")
 }
